dd8174f8cce69e2ef7bdcc772fe1cc31f3ef0b87
Wladimir Rocha <wld.rocha@gmail.com>
2022-06-22 16:15:58 -0400

    [ADD] login and logoute with functions

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,20 @@
-
+import { useNavigate } from 'react-router-dom'
 
 export const LoginPage = () => {
+  const navigate = useNavigate()
+  const handleLogin = () => {
+    navigate('/marvel', {
+      replace: true
+    })
+  }
+
   return (
-    <h1>LoginPage</h1>
+    <div className='container mt-5'>
+      <h1>LoginPage</h1>
+      <hr />
+      <button className='btn btn-primary' onClick={handleLogin}>
+        Login
+      </button>
+    </div>
   )
 }
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,13 @@
-import { Link, NavLink } from 'react-router-dom'
+import { useNavigate, Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    //con el objeto de replace, se reemplaza la navegacion anterior, por la nueva que se esta haciendo
+    navigate('/login', {
+      replace: true
+    })
+  }
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
       <Link className='navbar-brand' to='/'>
@@ -40,14 +47,12 @@ export const Navbar = () => {
 
       <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
         <ul className='navbar-nav ml-auto'>
-          <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link ${isActive ? 'active' : ''}`
-            }
-            to='/login'
+          <button
+            className="btn btn-primary"
+            onClick={handleLogout}
           >
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
