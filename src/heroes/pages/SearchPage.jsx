e55d809ee4c1ee78b3d8e667072e64217ce6b378
Wladimir Rocha <wld.rocha@gmail.com>
2022-07-18 20:26:23 -0400

    [ADD] markeup herpoage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,3 +1,35 @@
+import { HeroCard } from '../components'
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>
+  return (
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className='row'>
+        <div className='col-5'>
+          <h4>Searching</h4>
+          <form action=''>
+            <input
+              type='text'
+              placeholder='search hero'
+              name='searchText'
+              id='searchText'
+            />
+            <button className='btn btn-outline-primary mt-1'>Search</button>
+          </form>
+        </div>
+        <div className='col-7'>
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+          <div className="alert alert-danger">
+            No hero with <b>ABC</b>
+          </div>
+          {/* <HeroCard /> */}
+        </div>
+      </div>
+    </>
+  )
 }
